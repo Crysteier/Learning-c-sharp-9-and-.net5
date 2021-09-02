42a93df8f1277f700466d79873962698ed490198
Crysteier <crysteier12345@gmail.com>
2021-09-02 13:05:52 +0200

    Chapter 7 done
    i learned about the relationshiop between assemblies and namespaces and learned about porting existing .NET Framework code bases. Also I published my apps and libraries and deployed my code cross-platform.

---
+++ Chapter7/DotNetEverywhere/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace DotNetEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WriteLine("This app can run everywhere");
+            var name = ReadLine();
+            WriteLine($"Hi {name}");
+        }
+    }
+}
