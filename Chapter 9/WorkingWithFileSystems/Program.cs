b1ca1cd081ab8a959317246a734bc799a235fc33
Crysteier <crysteier12345@gmail.com>
2021-09-08 14:29:08 +0200

    Chapter 9 halfway
    need to commit so I can continue my work on my main computer

---
+++ Chapter 9/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace WorkingWithFileSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OutputFileSystemInfo();
+            //WorkWithDrives();
+            //WorkWithDirectories();
+            WorkWithFiles();
+        }
+
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0,-33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar);
+            WriteLine("{0,-33} {1}", "Directory.GetCurrentDirectory()", GetCurrentDirectory());
+            WriteLine("{0,-33} {1}", "Environment.CurrentDirectory", CurrentDirectory);
+            WriteLine("{0,-33} {1}", "Environment.SystemDirectory", SystemDirectory);
+            WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+            WriteLine("GetFolderPath((SpecialFolder)");
+            WriteLine("{0,-33} {1}", " .System", GetFolderPath(SpecialFolder.System));
+            WriteLine("{0,-33} {1}", " .ApplicationData", GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0,-33} {1}", " .MyDocuments", GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0,-33} {1}", " .Personal", GetFolderPath(SpecialFolder.Personal));
+        }
+
+        static void WorkWithDrives()
+        {
+            WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}", "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18:N0} | {4,18:N0}", drive.Name, drive.DriveType, drive.DriveFormat, drive.TotalSize, drive.AvailableFreeSpace);
+                }
+                else
+                {
+                    WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
+                }
+            }
+
+        }
+
+        static void WorkWithDirectories()
+        {
+            //define a directory path for a new folder startin in the users folder
+            var newFolder = Combine(GetFolderPath(SpecialFolder.Personal), "Code", "Chapter09", "New Folder");
+            WriteLine($"Working with: {newFolder}");
+
+            //check if it esxists
+            WriteLine($"Does the exist? {Exists(newFolder)}");
+
+            //create directory
+            WriteLine("Creating it ...");
+            CreateDirectory(newFolder);
+            WriteLine($"Does the exist? {Exists(newFolder)}");
+            Write("Confirm the directory exists, and then press ENTER: ");
+            ReadLine();
+
+            //delete directory
+            WriteLine("Deleting it...");
+            Delete(newFolder, recursive: true);
+            WriteLine($"Does the exist? {Exists(newFolder)}");
+        }
+
+        static void WorkWithFiles()
+        {
+            //define a directory name for the output files starting in the users personal
+            var dir = Combine(GetFolderPath(SpecialFolder.Personal), "Code", "Chapter09", "OutputFiles");
+            CreateDirectory(dir);
+
+            //define file paths
+            string textFile = Combine(dir, "Dummy.txt");
+            string backupFile = Combine(dir, "Dummy.bak");
+            WriteLine($"Working with: {textFile}");
+
+            //check if a file exists
+            WriteLine($"Does it exists? {File.Exists(textFile)}");
+
+            //create a new text file and write a line to it
+            StreamWriter textWriter = File.CreateText(textFile);
+            textWriter.WriteLine("Hello C#");
+            textWriter.Close(); //close the file and release resources
+            WriteLine($"Does it exists? {File.Exists(textFile)}");
+
+            //copy the file and overwrite if it already exists
+            File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+            WriteLine($"Does it exists? {File.Exists(backupFile)}");
+            Write("Confirm the file exists and then press ENTER: ");
+            ReadLine();
+
+            //delete file
+            File.Delete(textFile);
+            WriteLine($"Does it exists? {File.Exists(textFile)}");
+
+            //read from the backup file
+            WriteLine($"Readings from the backup file {backupFile}: ");
+            StreamReader textReader = File.OpenText(backupFile);
+            WriteLine(textReader.ReadToEnd());
+            textReader.Close();
+
+            //managing paths
+            WriteLine($"Folder anme: {GetDirectoryName(textFile)}");
+            WriteLine($"File name: {GetFileName(textFile)}");
+            WriteLine($"File name without extension: {GetFileNameWithoutExtension(textFile)}");
+            WriteLine($"File extension: {GetExtension(textFile)}");
+            WriteLine($"Random fil name: {GetRandomFileName()}");
+            WriteLine($"Temporary file name: {GetTempFileName()}");
+
+            //FileInfo stuff
+            var info = new FileInfo(backupFile);
+            WriteLine($"{backupFile}:");
+            WriteLine($"Conatins {info.Length} bytes");
+            WriteLine($"Last accesed {info.LastAccessTime}");
+            WriteLine($"Has readonly set to {info.IsReadOnly}");
+
+        }
+    }
+}
---
+++ Chapter 9/WorkingWithStreams/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using static System.Console;
+using System.Xml;
+using System.IO;
+using static System.IO.Path;
+using static System.Environment;
+using System.IO.Compression;
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        //Viper pilot call signs    
+        static string[] callsings = new string[]{
+        "Husker","Starbuck","Apollo","Boomer",
+        "Bulldog","Athena","Helo","Racetrack"
+            };
+        static void Main(string[] args)
+        {
+            //WorkWithText();
+            WorkWithXml();
+            WorkWithCompression();
+            WorkWithCompression(useBrotli: false);
+        }
+
+        static void WorkWithText()
+        {
+            //define a file to write to
+            string textFile = Combine(CurrentDirectory, "streams.txt");
+
+            //create a text file and return a helper writer
+            StreamWriter text = File.CreateText(textFile);
+
+            //enumerate the strings, writing each one to the stream on a seperate line
+            foreach (var item in callsings)
+            {
+                text.WriteLine(item);
+            }
+            text.Close();
+            WriteLine($"{textFile} cointains {new FileInfo(textFile).Length} bytes");
+            WriteLine(File.ReadAllText(textFile));
+        }
+
+        static void WorkWithXml()
+        {
+            FileStream xmlFileStream = null;
+            XmlWriter xml = null;
+
+            try
+            {
+                //define a file to write to
+                string xmlFile = Combine(CurrentDirectory, "streams.xml");
+                //create a file stream
+                xmlFileStream = File.Create(xmlFile);
+
+                //wrap the file stream in a xml writer helper and automatically indent nested elements
+                xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+                //write the xml declaration
+                xml.WriteStartDocument();
+
+                //write a root element
+                xml.WriteStartElement("callsigns");
+
+                //enumerate the strings writing each one to the stream
+                foreach (var item in callsings)
+                {
+                    xml.WriteElementString("callsigns", item);
+                }
+                //write the close root element
+                xml.WriteEndElement();
+
+                ///clore helper and stream
+                xml.Close();
+                xmlFileStream.Close();
+
+                //output alll the contents of the file
+                WriteLine($"{xmlFile} contains {new FileInfo(xmlFile).Length} bytes");
+                WriteLine(File.ReadAllText(xmlFile));
+            }
+            catch (Exception ex)
+            {
+                //if the path doesnt exist the etion will be caught
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            finally
+            {
+                if (xml != null)
+                {
+                    xml.Dispose();
+                    WriteLine("The XML writers unmanage dresources have been disposed");
+                }
+                if (xmlFileStream != null)
+                {
+                    xmlFileStream.Dispose();
+                    WriteLine("The file streams unmanaged resources have been disposed");
+                }
+            }
+        }
+
+        static void WorkWithCompression(bool useBrotli = true)
+        {
+            string fileExt = useBrotli ? "brotli" : "gzip";
+
+            string filePath = Combine(CurrentDirectory, $"streams.{fileExt}");
+
+            FileStream file = File.Create(filePath);
+
+            Stream compressor;
+            if (useBrotli)
+            {
+                compressor = new BrotliStream(file, CompressionMode.Compress);
+            }
+            else
+            {
+                compressor = new GZipStream(file, CompressionMode.Compress);
+            }
+
+            using (compressor)
+            {
+                using (XmlWriter xml = XmlWriter.Create(compressor))
+                {
+                    xml.WriteStartDocument();
+                    xml.WriteStartElement("callsigns");
+                    foreach (var item in callsings)
+                    {
+                        xml.WriteElementString("callsign", item);
+                    }
+
+                    //the normal call to WriteEndElement is not necessary
+                    //bacause when the XmlWriter disposes, it will 
+                    //automatically end any elements of any depth
+                }
+            }//also closes the underlying stream
+
+            //output all the contents of the compressed file
+            WriteLine($"{filePath} contains: {new FileInfo(filePath).Length}");
+            WriteLine("The compressed contents: ");
+            WriteLine(File.ReadAllText(filePath));
+
+            //read compressed files
+            WriteLine("Reading the compressed XML file:");
+            file = File.Open(filePath, FileMode.Open);
+
+            Stream decompressor;
+            if (useBrotli)
+            {
+                decompressor = new BrotliStream(file, CompressionMode.Decompress);
+            }
+            else
+            {
+                decompressor = new GZipStream(file, CompressionMode.Decompress);
+            }
+
+            using (decompressor)
+            {
+                using (XmlReader reader = XmlReader.Create(decompressor))
+                {
+                    while (reader.Read()) //read the next XML node
+                    {
+                        //check if we are on an element node named callsign
+                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+                        {
+                            reader.Read();//move to the text inside elemnt
+                            WriteLine($"{reader.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
